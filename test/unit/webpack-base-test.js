d8b06086a10f1b18c1c80dc16560f385e576eac5
chenkeyu <chenkeyu@heiyan.com>
2020-08-05 18:32:42 +0800

    assert的entry修改

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -1,10 +1,10 @@
 const assert = require('assert');
 
 describe('webpack.base.js test case', () => {
-  const baseConfig = require('../../lib/webpack.base.js');
+  const baseConfig = require('../../lib/webpack.base');
   console.log(baseConfig);
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, 'F:/newXM/new/builder-webpack/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, 'F:/newXM/new/builder-webpack/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index, 'F:/newXM/new/pushNpm/nymph-webpack/test/smoke/template/src/index/index.js');
+    assert.equal(baseConfig.entry.search, 'F:/newXM/new/pushNpm/nymph-webpack/test/smoke/template/src/search/index.js');
   });
 });
